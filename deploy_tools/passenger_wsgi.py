d2e6b2628184f0d4f970311a25a03fc49e1c4911
JStation <JStation@users.noreply.github.com>
2015-05-26 14:52:26 -0700

    generalized passenger_wsgi for deployment

--- deploy_tools/passenger_wsgi.py
+++ deploy_tools/passenger_wsgi.py
@@ -1,19 +1,19 @@
-# wsgi for running django at testing.dishduty.com
+# wsgi for running django on dreamhost
 
 import sys, os
 cwd = os.getcwd()
 sys.path.append(cwd)
 sys.path.append(cwd + '/source')
 
-os.environ['LD_LIBRARY_PATH'] = "/home/grak/testing.dishduty.com/virtualenv/lib"
+os.environ['LD_LIBRARY_PATH'] = cwd + "/virtualenv/lib"
 
-INTERP = "/home/grak/testing.dishduty.com/virtualenv/bin/python"
+INTERP = cwd + "/virtualenv/bin/python"
 if sys.executable != INTERP:
         os.execl(INTERP, INTERP, *sys.argv)
 
-sys.path.insert(0, '/home/grak/testing.dishduty.com/virtualenv/bin')
-sys.path.insert(0, '/home/grak/testing.dishduty.com/virtualenv/lib/python3.4/site-packages')
-sys.path.insert(0, '/home/grak/testing.dishduty.com/virtualenv/lib')
+sys.path.insert(0, cwd + '/virtualenv/bin')
+sys.path.insert(0, cwd + '/virtualenv/lib/python3.4/site-packages')
+sys.path.insert(0, cwd + '/virtualenv/lib')
 
 os.environ['DJANGO_SETTINGS_MODULE'] = "superlists.settings"
 
