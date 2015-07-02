7ea63b140482e9bc684dfdf06e503ea51a7933ff
JStation <JStation@users.noreply.github.com>
2015-07-02 09:41:14 -0700

    New FT for sharing, move session creation stuff to base

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,10 +1,16 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.conf import settings
+from .server_tools import create_session_on_server
+from .management.commands.create_session import create_pre_authenticated_session
+
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import WebDriverException
 import sys, os, time
 from datetime import datetime
 from .server_tools import reset_database
+
+
 
 DEFAULT_WAIT = 5
 SCREEN_DUMP_LOCATION = os.path.join(
@@ -111,3 +117,18 @@ class FunctionalTest(StaticLiveServerTestCase):
                 time.sleep(0.1)
         # final try, will raise any errors
         return function_with_assertion()
+
+    def create_pre_authenticated_session(self, email):
+        if self.against_staging:
+            session_key = create_session_on_server(self.server_host, email)
+        else:
+            session_key = create_pre_authenticated_session(email)
+
+        ## to set a cookie we need to first visit the domain.
+        ## 4040 pages load the quickest!
+        self.browser.get(self.server_url + "/404_no_such_url/")
+        self.browser.add_cookie(dict(
+            name=settings.SESSION_COOKIE_NAME,
+            value=session_key,
+            path='/',
+        ))
